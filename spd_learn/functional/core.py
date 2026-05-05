fix: exponent gradient in matrix_power backward

spd_learn/functional/core.py
@@ -423,9 +423,23 @@ class matrix_power(Function):
     def backward(ctx, grad_output):
         s, U, s_modified = ctx.saved_tensors
         exponent = ctx.exponent
-        return modeig_backward(
+        threshold = get_epsilon(s.dtype, "eigval_power")
+
+        # Gradient wrt the exponent
+        G = U.transpose(-1,-2) @ grad_output @ U
+        diag_G = torch.diagonal(G, dim1 = -2, dim2= -1)
+
+        s_safe = s.clamp(min = threshold)
+        exp_g = s_modified * torch.log(s_safe)
+
+        grad_exponent = torch.sum(diag_G * exp_g).view(1, 1)
+
+        # Gradient wrt X
+        grad_X = modeig_backward(
             grad_output, s, U, s_modified, matrix_power.derivative, exponent
-        ), None
+        )
+
+        return grad_X, grad_exponent
 
 
 class matrix_sqrt(Function):

tests/test_functional.py
@@ -122,7 +122,8 @@ def test_matrix_power():
 
     assert matrix_power.apply(X, 3.0).allclose(X @ X @ X)
     X.requires_grad_(True)
-    assert gradcheck(safe_matrix_power, (X, 0.1))
+    exponent = torch.tensor(0.1, dtype=torch.double, requires_grad=True)
+    assert gradcheck(safe_matrix_power, (X, exponent))
 
 
 def test_matrix_sqrt():